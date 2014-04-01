7968452989404c199ee3fb19a08613e477bf60b7
Bryce Davis <bad1128@gmail.com>
2014-04-01 13:30:07 -0400

    Begin board implementation

---
+++ board.rb
@@ -0,0 +1,21 @@
+class Board
+    attr_accessor :board
+
+    def initialize
+        @board = Array.new(6, Array.new(7, 0))
+    end
+
+    def to_s
+        ret = ""
+        @board.each do |row|
+            row.each do |col|
+                ret += "#{row[col]} "
+            end
+            ret += "\n"
+        end
+        return ret
+    end
+end
+
+b = Board.new
+puts b
